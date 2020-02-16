0ac95132ce1f14983c824b24622fc28dbf52314c
Kapil Gorve <kapil.gorve@instarem.com>
2020-02-16 12:33:29 +0530

    restore header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,37 +2,21 @@ import React from 'react'
 import { Link } from 'gatsby'
 import './header.css'
 
-const Header = () => {
-  if (typeof window === 'undefined') {
-    return null
-  }
-  window.omnisend = window.omnisend || [];
-  window.omnisend.push(["accountID", "5e48d82d4c7fa47e7cb56127"]);
-  window.omnisend.push(["track", "$pageViewed"]);
-
-  const init = () => {
-    var e = document.createElement("script");
-    e.type = "text/javascript"; e.async = !0;
-    e.src = "https://omnisrc.com/inshop/launcher-v2.js";
-    var t = document.getElementsByTagName("script")[0];
-    t.parentNode.insertBefore(e, t)
-  };
-  return (
-    <nav className="navbar">
-      <Link to="/" className="navbar-brand">
-        Home{init()}
-      </Link>
-      <Link to="/blog" className="navbar-brand">
-        Blog
+const Header = () => (
+  <nav className="navbar">
+    <Link to="/" className="navbar-brand">
+      Home
     </Link>
-      <Link to="/portfolio" className="navbar-brand">
-        Portfolio
+    <Link to="/blog" className="navbar-brand">
+      Blog
     </Link>
-      <Link to="/resume" className="navbar-brand">
-        Resume
+    <Link to="/portfolio" className="navbar-brand">
+      Portfolio
     </Link>
-    </nav>
-  );
-}
+    <Link to="/resume" className="navbar-brand">
+      Resume
+    </Link>
+  </nav>
+)
 
 export default Header
