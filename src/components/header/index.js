664c4a9199002d1b20c52c7ecdbbba7fe796170c
Kapil Gorve <kapil.gorve@instarem.com>
2020-02-16 11:32:13 +0530

    move to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { Link } from 'gatsby'
 import './header.css'
 
-const Header = () => (
+const Header = () => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+    window.omnisend = window.omnisend || [];
+    omnisend.push(["accountID", "5e48d82d4c7fa47e7cb56127"]);
+    omnisend.push(["track", "$pageViewed"]);
+    !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://omnisrc.com/inshop/launcher-v2.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
+
   <nav className="navbar">
     <Link to="/" className="navbar-brand">
       Home
@@ -17,6 +25,6 @@ const Header = () => (
       Resume
     </Link>
   </nav>
-)
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,6 @@ import YoutubeList from '../components/youtubeList'
 
 const IndexPage = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
-  if (typeof window === 'undefined') {
-    return null
-  }
-    window.omnisend = window.omnisend || [];
-    omnisend.push(["accountID", "5e48d82d4c7fa47e7cb56127"]);
-    omnisend.push(["track", "$pageViewed"]);
-    !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://omnisrc.com/inshop/launcher-v2.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
-
   return (
     <Layout>
       <Hero />
