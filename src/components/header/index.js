aebe77f1ce86489f2958cce349686af4c243da76
Kapil Gorve <kapil.gorve@instarem.com>
2020-02-16 11:40:19 +0530

    fix lint

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,25 +6,33 @@ const Header = () => {
   if (typeof window === 'undefined') {
     return null
   }
-    window.omnisend = window.omnisend || [];
-    omnisend.push(["accountID", "5e48d82d4c7fa47e7cb56127"]);
-    omnisend.push(["track", "$pageViewed"]);
-    !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://omnisrc.com/inshop/launcher-v2.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
+  window.omnisend = window.omnisend || [];
+  window.omnisend.push(["accountID", "5e48d82d4c7fa47e7cb56127"]);
+  window.omnisend.push(["track", "$pageViewed"]);
 
-  <nav className="navbar">
-    <Link to="/" className="navbar-brand">
-      Home
+  const init = () => {
+    var e = document.createElement("script");
+    e.type = "text/javascript"; e.async = !0;
+    e.src = "https://omnisrc.com/inshop/launcher-v2.js";
+    var t = document.getElementsByTagName("script")[0];
+    t.parentNode.insertBefore(e, t)
+  };
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">
+        Home{init()}
+      </Link>
+      <Link to="/blog" className="navbar-brand">
+        Blog
     </Link>
-    <Link to="/blog" className="navbar-brand">
-      Blog
+      <Link to="/portfolio" className="navbar-brand">
+        Portfolio
     </Link>
-    <Link to="/portfolio" className="navbar-brand">
-      Portfolio
+      <Link to="/resume" className="navbar-brand">
+        Resume
     </Link>
-    <Link to="/resume" className="navbar-brand">
-      Resume
-    </Link>
-  </nav>
+    </nav>
+  );
 }
 
 export default Header
