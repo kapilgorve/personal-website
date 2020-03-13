d444f54f6357ae9c3f19c051c6c7c6d1c4599fce
Kapil Gorve <kapil.gorve@instarem.com>
2020-03-13 19:17:07 +0530

    remove unused imports

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
