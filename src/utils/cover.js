7322867f7a4c63169a506029ec0885517fa7f8a3
Kapil Gorve <kapil.gorve@instarem.com>
2020-03-15 13:32:23 +0530

    return default og image if no image is presnet

--- src/utils/cover.js
+++ src/utils/cover.js
@@ -2,6 +2,6 @@ export function extractCoverUrl(html){
     if (html.includes('img')) {
         const rex = /<img[^>]+src="(https:\/\/[^">]+)"/g;
         const coverMatch = rex.exec(html)
-        return coverMatch ? coverMatch[1] : null;
+        return coverMatch ? coverMatch[1] : 'https://kapilgorve.s3.ap-south-1.amazonaws.com/blog/covers/Kapil+Gorve+www.jskap.com-site-cover.jpg';
       }
 }
