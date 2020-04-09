2d84015612efd6deb7ce2968842f8c0f25186e95
Kapil Gorve <kapil.gorve@instarem.com>
2020-04-09 11:13:49 +0530

    add resume seo

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../../components/layout'
+import SEO from '../../components/seo'
 
 const Resume = () => (
   <Layout>
+    <SEO
+        title="Kapil Gorve Resume"
+        description="Freelance ReactJs,Redux,NodeJs, React Native in Pune, India."
+        image="https://images.unsplash.com/photo-1586281380349-632531db7ed4?fit=crop&w=1200&h=630"
+        pathname="resume/"
+      />
     <div className="resume" />
-
     <iframe
       src="https://drive.google.com/file/d/1zRF8P8AOiy27hFat_VIaaiil9dngrCXr/preview"
       type="application/pdf"
