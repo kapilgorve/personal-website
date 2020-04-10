3f3a8dca47dca41e46d749c4ae50578e70c7aa7d
Kapil Gorve <kapil.gorve@instarem.com>
2020-04-11 01:29:50 +0530

    add description and tags to rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,11 +113,14 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
+                  description: edge.node.frontmatter.description,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
+                  custom_elements: [{
+                     "content:encoded": edge.node.html,
+                     "tags" : edge.node.frontmatter.tags,
+                     }],
                 })
               })
             },
