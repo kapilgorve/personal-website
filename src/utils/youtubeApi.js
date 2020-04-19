753a7b5caed58b970e32ae273d5375d481defd14
Kapil Gorve <kapil.gorve@instarem.com>
2020-04-19 16:53:05 +0530

    use a playlist to display videos

--- src/utils/youtubeApi.js
+++ src/utils/youtubeApi.js
@@ -1,5 +1,6 @@
 export async function getVideos() {
-    const url = 'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUAseX9IBXkoSZdxMX_41jag&key=AIzaSyCf5nsME4Fh9Io4qT5jCCCPB28iD7o_y5o&part=snippet&maxResults=50';
+    const playlistId = 'PLrm1PQpqTwnb69CNEGG5_MYERivErUDr0'; //name -Featured on Portfolio Site
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${playlistId}&key=AIzaSyCf5nsME4Fh9Io4qT5jCCCPB28iD7o_y5o&part=snippet&maxResults=50`;
     try {
         const response = await fetch(url);
         const { items } = await response.json();
