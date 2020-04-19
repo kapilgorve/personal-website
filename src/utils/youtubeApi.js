cb581e4191f34c2d2028c2820318ab537b11caec
Kapil Gorve <kapil.gorve@instarem.com>
2020-04-19 17:13:26 +0530

    use env variable for youtube

--- src/utils/youtubeApi.js
+++ src/utils/youtubeApi.js
@@ -1,6 +1,6 @@
 export async function getVideos() {
     const playlistId = 'PLrm1PQpqTwnb69CNEGG5_MYERivErUDr0'; //name -Featured on Portfolio Site
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${playlistId}&key=AIzaSyCf5nsME4Fh9Io4qT5jCCCPB28iD7o_y5o&part=snippet&maxResults=50`;
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${playlistId}&key=${process.env.GATSBY_YOUTUBE_KEY}&part=snippet&maxResults=50`;
     try {
         const response = await fetch(url);
         const { items } = await response.json();
