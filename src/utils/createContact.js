ad42c6e1f85ca65c215d319180df8bfce58863e6
Kapil Gorve <kapilsg186@gmail.com>
2021-04-18 23:43:25 +0530

    update contact form url

--- src/utils/createContact.js
+++ src/utils/createContact.js
@@ -1,5 +1,5 @@
 export async function createContact({email,name}) {
-    const url = `https://relaxed-banach-dcd3be.netlify.com/.netlify/functions/emailhandler?email=${email}&name=${name}`;
+    const url = `https://kapil-io-lambda-functions.netlify.app/.netlify/functions/emailhandler?email=${email}&name=${name}`;
         const response = await fetch(url);
         const res = await response.json();
         if(response.ok){
