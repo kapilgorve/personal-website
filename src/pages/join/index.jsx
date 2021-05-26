71278c5388f991c6b112381b1d82b6d425218cf0
Kapil Gorve <kapilsg186@gmail.com>
2021-05-26 22:38:00 +0530

    add og image

--- src/pages/join/index.jsx
+++ src/pages/join/index.jsx
@@ -4,7 +4,8 @@ import SEO from '../../components/seo'
 
 const Join = () => (
   <Layout>
-    <SEO title="Freelance Community" description="" pathname="join/" />
+    <SEO title="Freelance Community" description="" pathname="join/"
+    image="https://kapilgorve.s3.ap-south-1.amazonaws.com/blog/all/join-og.png" />
   </Layout>
 )
 
